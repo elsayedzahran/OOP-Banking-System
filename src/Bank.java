ca3347b1c8178d882613d436b56704343dafb41d
elsayedzahran <elsayedzahran789@gmail.com>
2023-05-25 01:55:28 +0300

    Initial commit

---
+++ src/Bank.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Bank {
+    private List<Account> accounts;
+
+    public Bank() {
+        accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+        System.out.println("Account created successfully!");
+    }
+
+    public Account findAccount(String accountNumber) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        return null;
+    }
+}
